dbb341d39ca38439857b3edda075f9d9bf498c00
Tim Moss <tmoss726@hotmail.com>
2019-10-28 20:49:30 -0400

    Updated components

--- src/App.js
+++ src/App.js
@@ -1,32 +1,35 @@
 import React from 'react';
 import { Layout, Header, Navigation, Drawer, Content } from 'react-mdl';
+import Main from './components/main';
+import { Link } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
     <div className="demo-big-content">
-		<Layout>
-			<Header title="Title" scroll>
-				<Navigation>
-					<a href="/">Link</a>
-					<a href="/">Link</a>
-					<a href="/">Link</a>
-					<a href="/">Link</a>
-				</Navigation>
-			</Header>
-			<Drawer title="Title">
-				<Navigation>
-					<a href="/">Link</a>
-					<a href="/">Link</a>
-					<a href="/">Link</a>
-					<a href="/">Link</a>
-				</Navigation>
-			</Drawer>
-			<Content>
-				<div className="page-content" />
-			</Content>
-		</Layout>
-	</div>
+      <Layout>
+        <Header title="Title" scroll>
+          <Navigation>
+            <Link to="/resume">Resume</Link>
+            <Link to="/about">About</Link>
+            <Link to="/projects">Projects</Link>
+            <Link to="/contact">Contact</Link>
+          </Navigation>
+        </Header>
+        <Drawer title="Title">
+          <Navigation>
+            <Link to="/resume">Resume</Link>
+            <Link to="/about">About</Link>
+            <Link to="/projects">Projects</Link>
+            <Link to="/contact">Contact</Link>
+          </Navigation>
+        </Drawer>
+        <Content>
+          <div className="page-content" />
+          <Main />
+        </Content>
+      </Layout>
+    </div>
   );
 }
 
--- src/components/contact.js
+++ src/components/contact.js
@@ -1,4 +1,4 @@
-import react, { Component } from "react";
+import React, { Component } from "react";
 
 class Contact extends Component {
   render() {
