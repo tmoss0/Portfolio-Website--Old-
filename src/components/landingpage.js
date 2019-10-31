5bb555971e99422dd5e9288b2e7935d77e8259ca
Tim Moss <tmoss726@hotmail.com>
2019-10-30 23:00:39 -0400

    Colors added, titles added

--- src/components/landingpage.js
+++ src/components/landingpage.js
@@ -1,10 +1,27 @@
 import React, { Component } from "react";
+import { Grid, Cell } from "react-mdl";
 
 class LandingPage extends Component {
   render() {
     return (
-      <div>
-        <h1>Landing Page</h1>
+      <div style={{ width: "100%", margin: "auto" }}>
+        <Grid className="landing-grid">
+          <Cell col={12}>
+            <img
+              src="http://styleguide.europeana.eu/images/fpo_avatar.png"
+              alt="avatar"
+              className="avatar-img"
+            />
+          </Cell>
+		  <div className="banner-text">
+			<h1>Full-Stack Web Development</h1>
+			<hr/>
+			<p>HTML/CSS | Bootstrap | JavaScript | ReactJS</p>
+		  </div>
+		  <div className="social-links">
+			  <a href=""></a>
+		  </div>
+        </Grid>
       </div>
     );
   }
