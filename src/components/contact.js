75047a71dfecbaafbc823170453a945fece7cc33
Tim Moss <tmoss726@hotmail.com>
2019-11-18 22:58:20 -0500

    Phone number, email added

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,5 +1,5 @@
-import React, { Component } from "react";
-import { Grid, Cell } from "react-mdl";
+import React, { Component } from 'react';
+import { Grid, Cell, List, ListItem, ListItemContent } from 'react-mdl';
 
 class Contact extends Component {
   render() {
@@ -11,9 +11,9 @@ class Contact extends Component {
             <img
               src="https://cdn2.iconfinder.com/data/icons/avatar-2/512/Fred_man-512.png"
               alt="avatar"
-              style={{ height: '250px' }}
+              style={{ height: "250px" }}
             />
-            <p style={{width: '50%', margin: 'auto', paddingTop: '1em'}}>
+            <p style={{ width: "50%", margin: "auto", paddingTop: "1em" }}>
               Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
               eiusmod tempor incididunt ut labore et dolore magna aliqua. Turpis
               tincidunt id aliquet risus feugiat. Lectus proin nibh nisl
@@ -21,8 +21,31 @@ class Contact extends Component {
             </p>
           </Cell>
           <Cell col={6}>
-			  
-		  </Cell>
+            <h2>Contact Me</h2>
+            <hr />
+            <div className="contact-list">
+              <List>
+                <ListItem>
+                  <ListItemContent
+                    className="list-item-content"
+                    style={{ fontSize: "25px", fontFamily: "Anton" }}
+                  >
+                    <i className="fa fa-phone-square" aria-hidden="true" />
+                    919-434-7783
+                  </ListItemContent>
+                </ListItem>
+                <ListItem>
+                  <ListItemContent
+                    className="list-item-content"
+                    style={{ fontSize: "25px", fontFamily: "Anton" }}
+                  >
+                    <i className="fa fa-envelope-square" aria-hidden="true" />
+                    tim@thmoss.com
+                  </ListItemContent>
+                </ListItem>
+              </List>
+            </div>
+          </Cell>
         </Grid>
       </div>
     );
