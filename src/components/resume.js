7a9e2666c87d49609e04fe62ba19220d3a162797
Tim Moss <tmoss726@hotmail.com>
2020-01-02 15:46:38 -0500

    CSS cleaned up, Resume tweaked

--- src/components/resume.js
+++ src/components/resume.js
@@ -5,16 +5,22 @@ import PDF from 'react-pdf-js-infinite';
 class Resume extends Component {	
 
 	render() {
-		return (    
-        <div className="resume">
-			<h2 className="page-banner-text">Resume</h2>
-		  <a href="https://github.com/tmoss0/resume/raw/master/Tim%20Moss%20Resume%20(Nov%202019).pdf">
-			<Button raised colored className="normal-text-transform">
-				Download Resume
-			</Button>
-		  </a>
-          <PDF className="resume-pdf" file="./Resume (Nov 2019).pdf" scale={1.25} />
+		return (
+      <div className="resume">
+        <h2 className="page-banner-text">Resume</h2>
+        <a href="https://github.com/tmoss0/resume/raw/master/Tim%20Moss%20Resume%20(Nov%202019).pdf">
+          <Button raised colored className="normal-text-transform">
+            Download Resume
+          </Button>
+        </a>
+        <div className="PDF">
+          <PDF
+            className="resume-pdf"
+            file="./Resume (Nov 2019).pdf"
+            scale={1.15}
+          />
         </div>
+      </div>
     );
 	}
 }
