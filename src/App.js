ccd984af1507469288a28dbf8cbb697b78d346e0
Tim Moss <tmoss726@hotmail.com>
2020-04-29 22:33:06 -0400

    Google Analytics test

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import ReactGA from 'react-ga';
 import { Layout, Header, Navigation, Drawer, Content } from 'react-mdl';
 import Main from './components/main';
 import { Link } from 'react-router-dom';
 import './App.scss';
 
 function App() {
+
+  ReactGA.initialize('UA-156654871-1');
+  ReactGA.pageview(window.location.pathname + window.location.search);
+
   return (
     <div className="demo-big-content">
       <Layout>
