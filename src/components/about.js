95586f167f94e0f9870f90a4c10d0bca8ab63ce2
Tim Moss <tmoss726@hotmail.com>
2020-05-10 17:03:49 -0400

    Update: text modififed

--- src/components/about.js
+++ src/components/about.js
@@ -17,43 +17,35 @@ class About extends Component {
         <Grid className="about-grid">
           <Cell>
             <p>
-              My adoration for programming started at a young age during the
-              loitering, dreaded dial-up years. The first language I programmed
-              in was Visual Basic, where I learned to creating interactive forms
-              while in middle school programming class. I fell in love with the
-              newfound capability to manipulate the contents on the screen.
-              Never in my wildest dreams did I think I'd still be programming
-              almost fifteen years later.
-            </p>
-            <p>
               I am an experienced Front-End Web Developer in Charlotte, NC with
-              a demonstrated history of nearly three years of working in the
-              marketing and advertising industry. I have built dynamics websites
-              for clients in various industries, from boutique bakeries to
-              high-end automotive detail shops and everything in between.
+              a demonstrated history of three years of working in the marketing
+              and advertising industry. I have built dynamics websites for
+              clients in numerous industries including boutique bakeries,
+              high-end automotive detail shops and a vehicle parts shop.
             </p>
             <p>
-              Utilizing HTML, CSS, JavaScript and PHP, I am constantly creating
+              Utilizing HTML, CSS, JavaScript and PHP, I love creating
               mobile-friendly websites for national clients and local
               entrepenuers. In addition to my programming experience, I also
               have experience working with numerous CMS platforms including
-              WordPress, Shopify and Adobe Experience Manager.
+              WordPress and Shopify.
             </p>
             <p>
-              One of my favorite aspects of programming is when I collaborate
-              with my clients in order to bring their dreams to life. Nothing is
-              more satisfying than meeting with a client in person and creating
-              a solution for their web-based needs.
+              One of my favorite aspects of programming doesn't actually involve
+              programming I enjoy being able to collaborate with my clients in
+              order to bring their dreams to life. Nothing is more satisfying
+              than meeting with a client in person, determining what kind of
+              market they are targeting, thencreating a solution for their
+              web-based needs.
             </p>
             <p>
-              While not on a computer, I am an avid Carolina Hurricanes
-              (#letsgocanes) and Chelsea FC fan. I also enjoy visiting the
-              various local breweries around Charlotte. I'm primarily a PC gamer
-              (although I do enjoy the occasional Mario Kart) and have
-              especially recently grown fond of virtual reality games.
+              I am also consistently learning new technologies and updating my
+              skillset with the newest web technologies, like React and VueJS. 
+			  If you have any questions, please reach out to me at
+              <a href="mailto:tim@thmoss.com">tim@thmoss.com</a>
             </p>
           </Cell>
-        </Grid>        
+        </Grid>
       </div>
     );
   }
