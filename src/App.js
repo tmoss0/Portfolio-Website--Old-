cbfc0245cbeb3dc716e3ba41b594a640efce9f26
Tim Moss <tmoss726@hotmail.com>
2020-05-19 11:02:35 -0400

    Feature: added drawer close when selecting page

--- src/App.js
+++ src/App.js
@@ -1,54 +1,57 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Layout, Header, Navigation, Drawer, Content } from 'react-mdl';
 import Main from './components/main';
 import { Link, NavLink } from 'react-router-dom';
 import './App.css';
 
-function App() {
+class App extends Component {	
 
-  return (
-    <div className="demo-big-content">
-      <Layout>
-        <Header className="header-color" title="Tim Moss" scroll>
-          <Navigation>
-            {/* <Link to="/">Home</Link>
-            <Link to="/resume">Resume</Link>
-            <Link to="/about">About</Link>
-            <Link to="/projects">Projects</Link>
-            <Link to="/contact">Contact</Link> */}
-            <NavLink exact to="/" activeClassName="selected">
-              Home
-            </NavLink>
-            <NavLink to="/resume" activeClassName="selected">
-              Resume
-            </NavLink>
-            <NavLink to="/about" activeClassName="selected">
-              About
-            </NavLink>
-            <NavLink to="/projects" activeClassName="selected">
-              Projects
-            </NavLink>
-            <NavLink to="/contact" activeClassName="selected">
-              Contact
-            </NavLink>
-          </Navigation>
-        </Header>
-        <Drawer title="Tim Moss">
-          <Navigation>
-            <Link to="/">Home</Link>
-            <Link to="/resume">Resume</Link>
-            <Link to="/about">About</Link>
-            <Link to="/projects">Projects</Link>
-            <Link to="/contact">Contact</Link>
-          </Navigation>
-        </Drawer>
-        <Content>
-          <div className="page-content" />
-          <Main />
-        </Content>
-      </Layout>
-    </div>
-  );
+	// Toggles drawer closed when selecting new page
+	hideToggle() {
+		var selectorId = document.querySelector('.mdl-layout');
+		selectorId.MaterialLayout.toggleDrawer();
+	}
+
+	render() {
+		return (		
+			<div className="demo-big-content">
+			<Layout>
+				<Header className="header-color" title="Tim Moss" scroll>
+					<Navigation>
+						<NavLink exact to="/" activeClassName="selected">
+						Home
+						</NavLink>
+						<NavLink to="/resume" activeClassName="selected">
+						Resume
+						</NavLink>
+						<NavLink to="/about" activeClassName="selected">
+						About
+						</NavLink>
+						<NavLink to="/projects" activeClassName="selected">
+						Projects
+						</NavLink>
+						<NavLink to="/contact" activeClassName="selected">
+						Contact
+						</NavLink>
+					</Navigation>
+				</Header>
+				<Drawer title="Tim Moss">
+					<Navigation>
+						<Link to="/" onClick={() => this.hideToggle()}>Home</Link>
+						<Link to="/resume" onClick={() => this.hideToggle()}>Resume</Link>
+						<Link to="/about" onClick={() => this.hideToggle()}>About</Link>
+						<Link to="/projects" onClick={() => this.hideToggle()}>Projects</Link>
+						<Link to="/contact" onClick={() => this.hideToggle()}>Contact</Link>
+					</Navigation>
+				</Drawer>
+				<Content>
+					<div className="page-content" />
+					<Main />
+				</Content>
+			</Layout>
+			</div>
+		);
+	}
 }
 
 export default App;
