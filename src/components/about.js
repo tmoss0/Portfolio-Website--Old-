bd1ef05a3087cd1c28b8ff83b8dae9deb617c11d
Tim Moss <tmoss726@hotmail.com>
2020-05-19 10:35:28 -0400

    Feature: copy update

--- src/components/about.js
+++ src/components/about.js
@@ -11,31 +11,29 @@ class About extends Component {
           <Cell col={12}>
             <p>
               I am an experienced Front-End Web Developer in Charlotte, NC with
-              a demonstrated history of three years of working in the marketing
-              and advertising industry. I have built dynamics websites for
-              clients in numerous industries including boutique bakeries,
-              high-end automotive detail shops and a vehicle parts shop.
+              a demonstrated history of three years working in the marketing
+              and advertising industry. I have built dynamic websites for
+              clients in numerous industries, including boutique bakeries,
+              high-end automotive detail shops and automtive parts shops.
             </p>
             <p>
               Utilizing HTML, CSS, JavaScript and PHP, I love creating
-              mobile-friendly websites for national clients and local
+              mobile and desktop usable websites for national clients and local
               entrepenuers. In addition to my programming experience, I also
               have experience working with numerous CMS platforms including
               WordPress and Shopify.
             </p>
             <p>
-              One of my favorite aspects of programming doesn't actually involve
-              programming I enjoy being able to collaborate with my clients in
-              order to bring their dreams to life. Nothing is more satisfying
-              than meeting with a client in person, determining what kind of
-              market they are targeting, thencreating a solution for their
-              web-based needs.
+              One part of the website creation process I enjoy is being 
+			  able to collaborate with my clients in order to bring their dreams 
+			  to life and help their business flourish. Nothing is more satisfying
+              than meeting with a client in person, understanding what market they
+			  are a part of, what clients they are targeting, then creating a solution 
+			  for their web-based needs.
             </p>
             <p>
-              I am also consistently learning new technologies and updating my
-              skillset with the newest web technologies, like React and VueJS.
               If you have any questions, please reach out to me at{' '}
-              <a href="mailto:tim@thmoss.com">tim@thmoss.com</a>
+              <a href="mailto:tim@thmoss.com">tim@thmoss.com</a> so we can connect.
             </p>
           </Cell>
         </Grid>
